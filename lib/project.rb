76cfb2426996eecd7b6a49f0707edc04caa364cb
Cody Alvarez <jedi.alvarez805@gmail.com>
2019-07-15 18:01:00 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,13 @@
+class Project
+    attr_accessor :backers, :title
+
+    def initialize(title)
+        @title = title
+        @backers = []
+    end
+
+    def add_backer(new_backer)
+        @backers << new_backer
+        new_backer.backed_projects << self
+    end
+end
